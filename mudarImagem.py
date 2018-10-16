cbdd8dd3f69d2582b854c47ba46514e676694f87
merlinuenp <josermerlin@gmail.com>
2018-10-16 18:47:31 -0300

    Mais exemplos.

--- mudarImagem.py
+++ mudarImagem.py
@@ -2,7 +2,7 @@ import pygame
 
 
 
-class Seta(pygame.sprite.Sprite):
+class Bicho(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.setaDireita = pygame.image.load("direita.png").convert_alpha()
@@ -16,9 +16,10 @@ class Seta(pygame.sprite.Sprite):
         tecla = pygame.key.get_pressed()
         if tecla[pygame.K_RIGHT]:
             self.image = self.setaDireita
+            self.rect.x += 2
         if tecla[pygame.K_LEFT]:
             self.image = self.setaEsquerda
-
+            self.rect.x -= 2
 
 
 # ----
@@ -28,12 +29,14 @@ ALTURA = 500
 sair = False
 
 pygame.init()
+clock = pygame.time.Clock()
 tela = pygame.display.set_mode((LARGURA, ALTURA))
-seta = Seta(250, 250 )
+seta = Bicho(250, 250 )
 objetos = pygame.sprite.Group()
 objetos.add(seta)
 
 while sair == False:
+    clock.tick(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sair = True
